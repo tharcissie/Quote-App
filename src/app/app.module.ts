3e081553f50073f62c2886afd08fdae258a1ce12
tharcissie <tharcissieidufashe@gmail.com>
2020-10-28 11:13:04 +0200

    added boostrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     QuoteDetailsComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DateCountPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,14 @@
 export class Quote {
-}
+    showDescription: boolean;
+
+    constructor(
+        public id: number, 
+        public quote: string, 
+        public author: string, 
+        public published: string, 
+        public completeDate: Date
+        )
+        {
+         this.showDescription=false;
+    }
+  }
